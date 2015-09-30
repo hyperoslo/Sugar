2317d03fa507e769bde89a7f4ec9236ae1651a4a
Christoffer Winterkvist <christoffer@hyper.no>
2015-09-30 16:43:52 +0200

    Refactor Array extension

--- Source/Extensions/Array+Queueable.swift
+++ Source/Extensions/Array+Queueable.swift
@@ -4,14 +4,12 @@ public protocol Queueable {
 
 public extension Array where Element : Queueable {
 
-  public mutating func processQueue(from: Int = 0, _ to: Int? = nil, process: ((object: Element) -> Void)? = nil) {
+  public mutating func processQueue(from: Int = 0, _ to: Int? = nil, process: ((element: Element) -> Void)? = nil) {
     let to = to != nil ? to! : count
     let currentQueue = self[from..<to]
-    for (index, queuable) in currentQueue.enumerate() {
-      if queuable.process() == true {
-        process?(object: queuable)
-        removeAtIndex(index - (currentQueue.count - self.count))
-      }
+    for (index, element) in currentQueue.enumerate() where element.process() {
+      process?(element: element)
+      removeAtIndex(index - (currentQueue.count - self.count))
     }
   }
 }
