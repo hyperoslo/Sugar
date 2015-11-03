15baec785050b9cc7044586838c0fab150e23054
Christoffer Winterkvist <christoffer@hyper.no>
2015-11-03 08:35:35 +0100

    Add operator ?=

--- Source/Operator.swift
+++ Source/Operator.swift
@@ -1,2 +1,13 @@
 import Foundation
 
+infix operator ?= { associativity right precedence 90 }
+
+func ?=<T>(inout left: T, right: T?) {
+  guard let value = right else { return }
+  left = value
+}
+
+func ?=<T>(inout left: T?, right: T?) {
+  guard let value = right else { return }
+  left = value
+}
