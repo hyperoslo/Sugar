7c566fa479f18ae5443c30bca870ec5ef543f26a
Christoffer Winterkvist <christoffer@winterkvist.com>
2015-11-05 15:22:32 +0100

    Make operator public

--- Source/Operator.swift
+++ Source/Operator.swift
@@ -2,12 +2,12 @@ import Foundation
 
 infix operator ?= { associativity right precedence 90 }
 
-func ?=<T>(inout left: T, right: T?) {
+public func ?=<T>(inout left: T, right: T?) {
   guard let value = right else { return }
   left = value
 }
 
-func ?=<T>(inout left: T?, right: T?) {
+public func ?=<T>(inout left: T?, right: T?) {
   guard let value = right else { return }
   left = value
 }
