7062265026013c9208fdfd216d524ecb9e1548e5
Vadym Markov <markov.vadym@gmail.com>
2015-12-15 16:33:18 +0100

    Add files to targets

--- Tests/TestQueueable.swift
+++ Tests/TestQueueable.swift
@@ -1,5 +1,6 @@
 import Foundation
 import XCTest
+@testable import Sugar
 
 struct Object: Queueable {
 
--- Tests/TestTruncate.swift
+++ Tests/TestTruncate.swift
@@ -1,5 +1,6 @@
 import Foundation
 import XCTest
+@testable import Sugar
 
 class StringTruncateTests: XCTestCase {
 
