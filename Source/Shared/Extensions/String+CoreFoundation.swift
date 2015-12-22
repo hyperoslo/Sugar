49fa9cc8fb79d5e0ec9d0494044590cda7d161d6
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-22 11:25:14 +0100

    Move truncate to core foundation extension

--- Source/Shared/Extensions/String+CoreFoundation.swift
+++ Source/Shared/Extensions/String+CoreFoundation.swift
@@ -8,4 +8,9 @@ extension String {
     return stringByReplacingOccurrencesOfString(string, withString: withString)
   }
 
+  func truncate(length: Int, suffix: String = "...") -> String {
+    guard self.characters.count > length else { return self }
+
+    return self.substringToIndex(self.startIndex.advancedBy(length)) + suffix
+  }
 }
