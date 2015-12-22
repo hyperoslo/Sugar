52225fd7b4305f2612b941d22a92bf05996eba51
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-22 11:22:14 +0100

    Add replace sugar

--- Source/Shared/Extensions/String+CoreFoundation.swift
+++ Source/Shared/Extensions/String+CoreFoundation.swift
@@ -3,4 +3,9 @@ import Foundation
 extension String {
 
   var length: Int { return characters.count }
+
+  func replace(string: String, with withString: String) -> String {
+    return stringByReplacingOccurrencesOfString(string, withString: withString)
+  }
+
 }
