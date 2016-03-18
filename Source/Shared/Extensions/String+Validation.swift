0e509e2efd1ba28482501adb62d3a3066944f052
Aashish Dhawan <aashishdhawan@gmail.com>
2016-03-18 18:07:26 +0530

    Added some Regex Validations

---
+++ Source/Shared/Extensions/String+Validation.swift
@@ -0,0 +1,24 @@
+//
+//  String+Validation.swift
+//  Sugar
+//
+//  Created by Aashish Dhawan on 18/03/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    
+    public func isEmail() -> Bool {
+        return matchesPattern(Regex.Email.pattern)
+    }
+    
+    public func isPhoneNumber() -> Bool {
+        return matchesPattern(Regex.PhoneNumber.pattern)
+    }
+    
+    public func isZipCode() -> Bool {
+        return matchesPattern(Regex.ZipCode.pattern)
+    }
+}
