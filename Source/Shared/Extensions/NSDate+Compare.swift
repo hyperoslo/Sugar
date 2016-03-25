2326cc186430c8c9353b16725ca809c3512548d9
Christoffer Winterkvist <christoffer@hyper.no>
2016-03-25 13:57:13 +0100

    Remove comment header

--- Source/Shared/Extensions/NSDate+Compare.swift
+++ Source/Shared/Extensions/NSDate+Compare.swift
@@ -1,11 +1,3 @@
-//
-//  NSDate+Compare.swift
-//  Sugar
-//
-//  Created by Aashish Dhawan on 18/03/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import UIKit
 
 extension NSDate : Comparable {}
