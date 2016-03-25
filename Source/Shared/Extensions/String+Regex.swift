b55769d954b4184896e272aeb41fb27394d3c18e
Aashish Dhawan <aashishdhawan@gmail.com>
2016-03-25 20:44:05 +0530

    Resolved review comments

--- Source/Shared/Extensions/String+Regex.swift
+++ Source/Shared/Extensions/String+Regex.swift
@@ -1,21 +1,9 @@
-//
-//  String+Regex.swift
-//  Sugar
-//
-//  Created by Aashish Dhawan on 18/03/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
 
 public enum Regex: String {
     
     case Email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
-    case PhoneNumber = "^[0-9]{10}$"
-    case ZipCode = "^[0-9]{5}$"
-    case TwoDecimalPoints = "^[0-9]*((\\.)[0-9]{0,2})?$"
-    case TwoDecimalPointsAndTwoDigits = "^[0-9]{0,2}((\\.)[0-9]{0,2})?$"
-    case Numbers = "^[0-9]*?$"
+    case Number = "^[0-9]*?$"
     
     var pattern: String {
         return rawValue
@@ -24,7 +12,7 @@ public enum Regex: String {
 
 public extension String {
     
-    public func matchesPattern(pattern: String) -> Bool {
+    public func match(pattern: String) -> Bool {
         do {
             let regex = try NSRegularExpression(pattern: pattern, options: [.CaseInsensitive])
             return regex.firstMatchInString(self, options: NSMatchingOptions(rawValue: 0), range: NSMakeRange(0, characters.count)) != nil
--- Source/Shared/Extensions/String+Validation.swift
+++ Source/Shared/Extensions/String+Validation.swift
@@ -1,28 +1,12 @@
-//
-//  String+Validation.swift
-//  Sugar
-//
-//  Created by Aashish Dhawan on 18/03/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
 
 public extension String {
     
     public func isEmail() -> Bool {
-        return matchesPattern(Regex.Email.pattern)
-    }
-    
-    public func isPhoneNumber() -> Bool {
-        return matchesPattern(Regex.PhoneNumber.pattern)
-    }
-    
-    public func isZipCode() -> Bool {
-        return matchesPattern(Regex.ZipCode.pattern)
+        return match(Regex.Email.pattern)
     }
     
     public func isNumber() -> Bool {
-        return matchesPattern(Regex.Numbers.pattern)
+        return match(Regex.Number.pattern)
     }
 }
