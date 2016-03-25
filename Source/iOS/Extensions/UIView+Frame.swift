24e33a5980d1fc1e288cc2f942a39f7767775159
Aashish Dhawan <aashishdhawan@gmail.com>
2016-03-25 21:12:46 +0530

    Make working with UIView frame super easy

---
+++ Source/iOS/Extensions/UIView+Frame.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+public extension UIView {
+    
+    public var height: CGFloat {
+        get {
+            return CGRectGetHeight(frame)
+        }
+        set {
+            frame.size.height = newValue
+        }
+    }
+    
+    public var width: CGFloat {
+        get {
+            return CGRectGetWidth(frame)
+        }
+        set {
+            frame.size.width = newValue
+        }
+    }
+    
+    public var x: CGFloat {
+        get {
+            return CGRectGetMinX(frame)
+        }
+        set {
+            frame.origin.x = newValue
+        }
+    }
+    
+    public var y: CGFloat {
+        get {
+            return CGRectGetMinY(frame)
+        }
+        set {
+            frame.origin.y = newValue
+        }
+    }
+}
+
