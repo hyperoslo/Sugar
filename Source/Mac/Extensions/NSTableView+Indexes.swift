1a4e4618e3729c5ebd0b7376bbac7c2934000753
Christoffer Winterkvist <christoffer@hyper.no>
2016-05-18 21:56:58 +0200

    Update NSTableView extension

--- Source/Mac/Extensions/NSTableView+Indexes.swift
+++ Source/Mac/Extensions/NSTableView+Indexes.swift
@@ -2,27 +2,32 @@ import Cocoa
 
 public extension NSTableView {
 
-  func insert(indexes: [Int], section: Int = 0, animation: NSTableViewAnimationOptions = .EffectFade) {
+  func insert(indexes: [Int], section: Int = 0, animation: NSTableViewAnimationOptions = .EffectFade, completion: (() -> Void)? = nil) {
     let indexPaths = NSMutableIndexSet()
     indexes.forEach { indexPaths.addIndex($0) }
-    performUpdates { insertRowsAtIndexes(indexPaths, withAnimation: animation) }
+    performUpdates({ insertRowsAtIndexes(indexPaths, withAnimation: animation) },
+                   endClosure: completion)
+
   }
 
-  func reload(indexes: [Int], section: Int = 0, animation: NSTableViewAnimationOptions = .EffectFade) {
+  func reload(indexes: [Int], section: Int = 0, animation: NSTableViewAnimationOptions = .EffectFade, completion: (() -> Void)? = nil) {
     let indexPaths = NSMutableIndexSet()
     indexes.forEach { indexPaths.addIndex($0) }
-    performUpdates { reloadDataForRowIndexes(indexPaths, columnIndexes: NSIndexSet(index: 0)) }
+    performUpdates({ reloadDataForRowIndexes(indexPaths, columnIndexes: NSIndexSet(index: 0)) },
+                   endClosure: completion)
   }
 
-  func delete(indexes: [Int], animation: NSTableViewAnimationOptions = .EffectFade) {
+  func delete(indexes: [Int], animation: NSTableViewAnimationOptions = .EffectFade, completion: (() -> Void)? = nil) {
     let indexPaths = NSMutableIndexSet()
     indexes.forEach { indexPaths.addIndex($0) }
-    performUpdates { removeRowsAtIndexes(indexPaths, withAnimation: animation) }
+    performUpdates({ removeRowsAtIndexes(indexPaths, withAnimation: animation) },
+                   endClosure: completion)
   }
 
-  private func performUpdates(@noescape closure: () -> Void) {
+  private func performUpdates(@noescape closure: () -> Void, endClosure: (() -> Void)? = nil) {
     beginUpdates()
     closure()
     endUpdates()
+    endClosure?()
   }
 }
