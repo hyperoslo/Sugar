06484480035f90da187a6f42cc52b9ee9394f419
Khoa Pham <onmyway133@gmail.com>
2017-04-26 11:44:07 +0200

    Add Once

---
+++ Source/Shared/Once.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class Once {
+
+  var already: Bool = false
+
+  func run(_ block: () -> Void) {
+    guard !already else {
+      return
+    }
+
+    block()
+    already = true
+  }
+}
