e5255ca2c85d539140737bd3dfac2c03a2022f33
Khoa Pham <onmyway133@gmail.com>
2017-06-16 14:09:06 +0200

    Remove sizes

--- Source/iOS/Extensions/UIDevice+Model.swift
+++ Source/iOS/Extensions/UIDevice+Model.swift
@@ -1,38 +1,6 @@
 import UIKit
 
 public extension UIDevice {
-
-  public enum DeviceKind {
-    case iPhone4
-    case iPhone5
-    case iPhone6
-    case iPhone6Plus
-    case iPad
-    case unknown
-  }
-
-  public var kind: DeviceKind {
-    guard userInterfaceIdiom == .phone else {
-      return .iPad
-    }
-
-    let result: DeviceKind
-
-    switch UIScreen.main.nativeBounds.height {
-    case 960:
-      result = .iPhone4
-    case 1136:
-      result = .iPhone5
-    case 1334:
-      result = .iPhone6
-    case 2208:
-      result = .iPhone6Plus
-    default:
-      result = .unknown
-    }
-
-    return result
-  }
     
   public static func isPhone() -> Bool {
     return UIDevice().userInterfaceIdiom == .phone
