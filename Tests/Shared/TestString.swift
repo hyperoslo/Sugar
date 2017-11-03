020e563cf26c82d12879314dbd2d9f5421f6130f
Khoa Pham <onmyway133@gmail.com>
2017-11-03 13:42:54 +0100

    Fix tests

--- Tests/Shared/TestString.swift
+++ Tests/Shared/TestString.swift
@@ -6,19 +6,6 @@ class StringTests: XCTestCase {
 
   let testString = "John Hyperseed"
 
-  func testTruncate() {
-    XCTAssertEqual(testString.truncate(4), "John...")
-    XCTAssertNotEqual(testString.truncate(5), "John...")
-  }
-
-  func testTruncateSuffix() {
-    XCTAssertEqual(testString.truncate(4, suffix: "-"), "John-")
-  }
-
-  func testTruncateWithShortString() {
-    XCTAssertEqual(testString.truncate(20), testString)
-  }
-
   func testUppercase() {
     XCTAssertEqual("hello".uppercaseFirstLetter, "Hello")
     XCTAssertEqual("".uppercaseFirstLetter, "")
