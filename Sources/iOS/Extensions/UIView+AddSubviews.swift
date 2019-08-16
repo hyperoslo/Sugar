2bb612fdfcad5ec8c228560d80c03f90dbeeec5d
Johannes Ebeling <johannes.ebeling@wandelbots.com>
2019-08-17 00:56:17 +0200

    Mirror api for adding arranged subviews to stackviews

--- Sources/iOS/Extensions/UIView+AddSubviews.swift
+++ Sources/iOS/Extensions/UIView+AddSubviews.swift
@@ -5,4 +5,8 @@ public extension UIView {
   func addSubviews(_ subviews: UIView...) {
     subviews.forEach { addSubview($0) }
   }
+    
+  func addSubviews(contentsOf sequence: [UIView]) {
+    sequence.forEach { addSubview($0) }
+  }
 }
