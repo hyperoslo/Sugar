e0bca831988b1c036f0160186294c2a343530ab1
janlionly <janlionly@gmail.com>
2020-06-15 16:58:53 +0800

    fix: add ios version to package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Sugar",
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
