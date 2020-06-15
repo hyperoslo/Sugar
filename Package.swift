a7a48df258d308bbdf9b8451aa73ceb1a9af2663
janlionly <janlionly@gmail.com>
2020-06-15 17:03:04 +0800

    docs: change ios version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Sugar",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
